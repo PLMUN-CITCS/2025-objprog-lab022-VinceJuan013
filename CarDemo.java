ef5be41811df729460851cafde5f0a46675aecbc
Viel <juanvinceruel_bsit@plmun.edu.ph>
2025-02-25 17:12:49 +0800

    Submitting OBJPROG Week 05 - Laboratory # 22

---
+++ CarDemo.java
@@ -0,0 +1,26 @@
+class Car {
+    // Attributes (data fields)
+    String make;
+    String model;
+    int year;
+
+    // Method to display car information
+    public void displayInfo() {
+        System.out.println("Car: " + year + " " + make + " " + model);
+    }
+}
+
+public class CarDemo {
+    public static void main(String[] args) {
+        // Create a Car object
+        Car myCar = new Car();
+        
+        // Assign values to attributes
+        myCar.make = "Toyota";
+        myCar.model = "Corolla";
+        myCar.year = 2020;
+        
+        // Call the displayInfo method
+        myCar.displayInfo();
+    }
+}
